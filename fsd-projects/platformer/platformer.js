b171e432f885e57ed9c1f1b79a4d787561740aa3
JadenGPerkins <jadenperkins13@icloud.com>
2025-06-09 15:22:08 +0000

    Finalized Platformer game

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,22 +27,52 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     
+     toggleGrid();
 
 
     // TODO 2 - Create Platforms
 
-
-
-
+     createPlatform(600, 110, 20, 475, "black"); 
+     createPlatform(585, 110, 50, 20, "grey");
+     createPlatform(1300, 650, 100, 20, "brown");  //plat one
+     createPlatform(1150, 525, 100, 20, "brown");  //plat 2
+     createPlatform(1000, 450, 100, 20, "brown");    //plat 3
+     createPlatform(500, 575, 200, 50, "yellow");   //cross plat under red
+     createPlatform(800, 325, 100, 20, "brown");   //plat 4
+     createPlatform(900, 550, 50, 20, "brown");    //plat under 4-5
+     createPlatform(1100, 250, 300, 20, "brown");   // plat 5 
+     createPlatform(450, 225, 300, 20, "gray");  // cross plat top
+     createPlatform(100, 200, 25, 20, "black");     // plat 6 "high up"
+     createPlatform(200, 400, 25, 20, "black");     // plat 7 middle
+     createPlatform(300, 150, 25, 20, 'black');     // plat 8 bottom
+     createPlatform(500, 225, 100, 225, "white"); // player blocker left
+     createPlatform(600, 225, 100, 225, "white"); // player blocker right
     // TODO 3 - Create Collectables
-
+    (createCollectable("diamond", 100, 100, 0.5, 1)) //collectable 3 
+    createCollectable("grace", 550, 500, 0.5, 0.7); //collectable 4
+    createCollectable("max", 625, 500, 0.5, 0.5);  //col;lectable 1
+    createCollectable("steve", 1350, 100, 0.5, 1); //collectable 2
 
 
     
     // TODO 4 - Create Cannons
-
-
+    createCannon("top", 200, 40000);
+    createCannon("top", 300, 40000);
+    createCannon("top", 400, 40000);
+    createCannon("top", 500, 40000);
+    createCannon("top", 600, 40000);
+    createCannon("top", 700, 40000);
+    createCannon("top", 800, 40000);
+    createCannon("top", 900, 40000);
+    createCannon("top", 1000, 40000);
+    createCannon("top", 1200, 40000);
+    createCannon("top", 1300, 40000);
+    createCannon("top", 1100, 40000);
+    createCannon("right", 750, 2500);
+    createCannon("right", 450, 4000);
+    createCannon("left", 475, 6500);
+    createCannon("left", 50, 5500);
     
     
     //////////////////////////////////
